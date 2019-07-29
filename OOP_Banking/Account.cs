e1a2db527fe765bc41fa5011e14bf79c2ede009b
max-student@outlook.com <max-student@outlook.com>
2019-07-29 12:28:15 -0400

    Add project files.

---
+++ OOP_Banking/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Banking {
+    public class Account {
+        public string Number { get; set; }
+        private decimal Balance { get; set; } = 0;
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public void Deposit(decimal Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine($"Deposit amount must be GT zero.");
+            }
+            else {
+                this.Balance += Amount;          
+            }            
+        }
+        public void Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+            Console.WriteLine($"Withdraw amount must be GT zero.");
+            }
+            else {
+            if (this.Balance >= Amount) {
+                this.Balance -= Amount;
+            }
+        }
+    }
+        public decimal GetBalance() {
+             return this.Balance;
+        }
+    }
+
+  }
+
+
---
+++ OOP_Banking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP_Banking {
+    class Program {
+        static void Main(string[] args) {
+            Account acct = new Account();
+            acct.Number = "1001";
+            acct.Name = "Test Account";
+            acct.Deposit(20);
+            acct.Withdraw(10);
+            decimal  balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (Should be 10)");
+            acct.Deposit(-10);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (Should be 10)");
+            acct.Withdraw(20);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (Should be 10)");
+
+
+        }
+    }
+}
